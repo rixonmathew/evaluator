7a1363580a509f530dc395b2df2f6cddf81b48d8
Rixon Mathew <rixonmathew@gmail.com>
2015-01-04 11:18:27 +0530

    Added the audio files

--- controller/custom/AudioSection1Renderer.php
+++ controller/custom/AudioSection1Renderer.php
@@ -2,11 +2,8 @@
 
 class AudioSection1Renderer {
 
-    public function doEvaluate($question) {
-        //TODO add hindi and marathi audio using radio boxes
-        $renderingHTML = <<<RENDER_HTML
-        <div id="questionSet" class="row">
-            <div class="col-md-5">
+    /*
+     * html to switch between hindi and marathi
                 <div class="radio">
                    <label class="radio-inline">Select the language for the audio:</label>
                    <label class="radio-inline">
@@ -16,15 +13,40 @@ class AudioSection1Renderer {
                       <input type="radio" name = "audioLanguage" id="audioLanguage" value="marathi" onclick="flipAudioLanguage('marathi')">Marathi
                    </label>
                 </div>
-                <audio controls id="audio_question_1" name="audio_question_1">
-                    <source id="audio_file_1" src="media/audio_hindi.mp3" type="audio/mp3"/>
-                    Your browser does not support audio control
-                </audio>
+     */
+
+    public function doEvaluate($question) {
+        //TODO add hindi and marathi audio using radio boxes
+        $audioControlHTML = $this->getHTMLforAudioControls();
+
+        $renderingHTML = <<<RENDER_HTML
+        <div id="questionSet" class="row">
+            <div class="col-md-5">
+                $audioControlHTML
                 <h4>{$question->getText()}</h4>
                 <textarea rows="3" cols="70" name="{$question->getId()}_answer" id="{$question->getId()}_answer" class="form-control" required="true"></textarea>
             </div>
         </div>
 RENDER_HTML;
         return $renderingHTML.PHP_EOL;
+    }
+
+    private function getHTMLforAudioControls() {
+        $audioControlHTML=<<<AUDIO_HTML
+                <audio controls id="audio_question_1" name="audio_question_1">
+                    <source id="audio_file_1" src="media/word1.mp3" type="audio/mp3"/>
+                    Your browser does not support audio control
+                </audio>
+AUDIO_HTML;
+
+        for ($i=2;$i<=15;$i++) {
+        $audioControlHTML.=<<<AUDIO_HTML_LOOP
+                <audio controls id="audio_question_$i" name="audio_question_$i">
+                    <source id="audio_file_$i" src="media/word$i.mp3" type="audio/mp3"/>
+                    Your browser does not support audio control
+                </audio>
+AUDIO_HTML_LOOP;
+        }
+        return $audioControlHTML;
     }
 }
